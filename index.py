698d9f6d4dda269aff9eb2717656759ef91dc58e
Smoke <smoke@MacBook-Air-de-Agustin.local>
2021-09-25 01:30:48 -0300

    version con jupyter modificado

--- index.py
+++ index.py
@@ -3,12 +3,16 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+import seaborn as sns
 
 
-aplicaciondb = pd.read_csv('Aplicaciones.csv')
+
+aplicaciondb = pd.read_csv('comidas.csv')
 ##aplicacionfiltro = aplicaciondb.loc[(aplicaciondb['Nombre y apellido'] == 'Flavio Rodriguez'),["Nombre y apellido", "Bebida"]]
 ##print(aplicacionfiltro)
 ##aplicacionfiltro.to_csv('agustinjugo.csv')
 
-fig = px.bar(aplicaciondb, x="Aplicacion", y="Tiempo", color="Nombre", barmode="group")
+fig = px.bar(aplicaciondb, x="Comida", y="Cronometro", color="Nombre y apellido", barmode="group")
 fig.show()
+tips = sns.load_dataset("aplicaciondb")
+sns.catplot(x="Comida", kind="count", palette="ch:.25", data=tips)
