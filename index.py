21fdaddb307ee14c961c0d2d76dc65f6afb59294
Agustin Flavio Rodriguez <smoke@MacBook-Air-de-Agustin.local>
2021-09-25 00:35:05 -0300

    pusheando

--- index.py
+++ index.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 
 
-aplicaciondb = pd.read_csv('Aplicacion.csv')
+aplicaciondb = pd.read_csv('Aplicaciones.csv')
 ##aplicacionfiltro = aplicaciondb.loc[(aplicaciondb['Nombre y apellido'] == 'Flavio Rodriguez'),["Nombre y apellido", "Bebida"]]
 ##print(aplicacionfiltro)
 ##aplicacionfiltro.to_csv('agustinjugo.csv')
 
-fig = px.bar(aplicaciondb, x="Aplicacion", y="Cant de aperturas", color="Nombre", barmode="group")
+fig = px.bar(aplicaciondb, x="Aplicacion", y="Tiempo", color="Nombre", barmode="group")
 fig.show()
